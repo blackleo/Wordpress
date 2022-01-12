481f210c636fb8e51b60c9aa0a8f2fde44b23a5e
Mehul Dave <mehuldave13@gmail.com>
2022-01-13 00:02:29 +0530

    Update settings_page.php

--- delivery_time_for_woocommerce/settings_page.php
+++ delivery_time_for_woocommerce/settings_page.php
@@ -5,11 +5,17 @@
 		$days = $_POST['delivery_time'];
 		$display_on = $_POST['display_on']; 
 		$color = $_POST['color'];
-		$updateDeliverySettings = array('days' => $days, 'display_on' => $display_on, 'color' => $color );
+		$delivery_description = $_POST['delivery_description'];
+		$updateDeliverySettings = array('days' => $days, 'display_on' => $display_on, 'color' => $color, 'description' => $delivery_description);
 		update_option('delivery_time_options', $updateDeliverySettings);
 		header('Location: '.$_SERVER['REQUEST_URI']);
  	}
 ?>
+<style>
+    .woocommerce-save-button{
+        display:none!important;
+    }
+</style>
 <div class="wpbody-content">
 	<h1>Delivery Time for WooCommerce</h1>
 	<form action="" method="POST">
@@ -17,6 +23,10 @@
 			<tr>
 				<th scope="row"><label>Delivery time</label></th>
 				<td><input type="text" name="delivery_time" placeholder="day" value="<?php echo $getData['days']; ?>"></td>
+			</tr>
+			<tr>
+				<th scope="row"><label>Delivery description</label></th>
+				<td><textarea name="delivery_description" rows="5" cols="100"><?php echo $getData['description']; ?></textarea></td>
 			</tr>
 			<tr>
 				<th scope="row"><label>Delivery on</label></th>
